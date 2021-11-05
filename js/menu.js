f516b5749b728a195c480c45c7fe3d915adfaf5e
vavrinuknatala <vavrinuknatala@gmail.com>
2021-11-05 23:05:04 +0200

    задание 8

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector('.header__mobile-menu-btn'),
-    closeMenuBtn: document.querySelector('.mobile-menu__close-btn'),
-    modal: document.querySelector('.mob-menu'),
-  };
+    const refs = {
+        openMenuBtn: document.querySelector('.menu-open-btn'),
+        closeMenuBtn: document.querySelector('.menu-close-btn'),
+        menu: document.querySelector('.mob-menu'),
+    };
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-  }
+    function toggleMenu() {
+        refs.menu.classList.toggle('is-hidden');
+    }
 })();
